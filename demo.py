bb59975e1faf10ba32556462a59aa8bb6d86e885
Arshan A Shaik <arshanshaik1199@gmail.com>
2025-06-14 14:16:03 +0530

    added embedding generation for query, .txt, .pdf and word

--- demo.py
+++ demo.py
@@ -1,2 +1,7 @@
 from language_model.deepseek import get_openrouter_response
-print(get_openrouter_response("who is naruto", temperature=0.5, max_tokens = 25))
+# print(get_openrouter_response("who is naruto", temperature=0.5, max_tokens = 25))
+
+from embedding_model.embed_query import query_embedding, document_embedding
+# print(query_embedding("who is naruto"))
+
+print(document_embedding(r"C:\Users\ARSHAN\Desktop\sohan2\ragna\samples\sample.pdf")[0])
